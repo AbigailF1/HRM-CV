25ebb96373874fb31cd4c5f531ffffd87e13fe93
nathanaelcheramlak <nathanaelcheramlak7@gmail.com>
2026-05-03 17:43:26 +0300

    chore(web): scaffold React Vite app

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,21 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const queryClient = new QueryClient()
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <div>HRM Web</div>,
+	},
+])
+
+function App() {
+	return (
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+		</QueryClientProvider>
+	)
+}
+
+export default App
